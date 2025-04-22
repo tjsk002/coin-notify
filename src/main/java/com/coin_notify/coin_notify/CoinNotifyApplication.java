e31c6ec9a66d1e3b5ccb919f9bd56849e8c00f53
tjsk002 <tjsk002@naver.com>
2025-04-22 13:39:13 +0900

    init: initial commit

---
+++ src/main/java/com/coin_notify/coin_notify/CoinNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.coin_notify.coin_notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinNotifyApplication.class, args);
+	}
+
+}
