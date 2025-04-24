dd0ae460b260a9864b8ede9d25381e2206725e30
tjsk002 <tjsk002@naver.com>
2025-04-24 11:17:41 +0900

    chore: r2dbc 세팅

---
+++ src/main/java/com/coin_notify/coin_notify/config/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package com.coin_notify.coin_notify.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+@Configuration
+public class DatabaseConfig {
+
+	@Bean
+	public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+		initializer.setConnectionFactory(connectionFactory);
+
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator(new ClassPathResource("db/data.sql"));
+		initializer.setDatabasePopulator(populator);
+
+		return initializer;
+	}
+}
