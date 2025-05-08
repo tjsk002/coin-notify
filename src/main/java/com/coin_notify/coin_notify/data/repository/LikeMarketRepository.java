9f63b8d13feaa6e7c16e61d250983386e1c79456
tjsk002 <tjsk002@naver.com>
2025-05-08 14:08:35 +0900

    feat: coin, market Entity, Repository 수정

--- src/main/java/com/coin_notify/coin_notify/data/repository/LikeMarketRepository.java
+++ src/main/java/com/coin_notify/coin_notify/data/repository/LikeMarketRepository.java
@@ -2,8 +2,10 @@ package com.coin_notify.coin_notify.data.repository;
 
 import com.coin_notify.coin_notify.data.entity.LikeMarketEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface LikeMarketRepository extends ReactiveCrudRepository<LikeMarketEntity, Long> {
 	Mono<LikeMarketEntity> findByUserIdAndMarketId(Long userId, Long marketId);
+	Flux<LikeMarketEntity> findByIsActiveTrue();
 }
