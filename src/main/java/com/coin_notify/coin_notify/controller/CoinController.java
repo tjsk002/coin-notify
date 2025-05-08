439d12415aceffaeaf0c893352cd2cfb309c2bef
tjsk002 <tjsk002@naver.com>
2025-05-08 14:05:43 +0900

    feat: 코인 목록 API

--- src/main/java/com/coin_notify/coin_notify/controller/CoinController.java
+++ src/main/java/com/coin_notify/coin_notify/controller/CoinController.java
@@ -1,11 +1,16 @@
 package com.coin_notify.coin_notify.controller;
 
+import com.coin_notify.coin_notify.data.dto.CoinResponseDto;
+import com.coin_notify.coin_notify.response.ApiResponse;
 import com.coin_notify.coin_notify.service.CoinService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 public class CoinController {
@@ -19,5 +24,10 @@ public class CoinController {
 	@GetMapping("/coin-collect")
 	public Mono<ResponseEntity<String>> process() {
 		return coinService.coinCollectionProcess().thenReturn(ResponseEntity.ok("ok"));
+	}
+
+	@GetMapping("/coin-list")
+	public Mono<ResponseEntity<ApiResponse<List<CoinResponseDto>>>> coinList(HttpServletRequest request) {
+		return coinService.coinList(request);
 	}
 }
