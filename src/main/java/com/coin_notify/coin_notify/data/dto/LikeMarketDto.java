ebd811649b94070d9f684cf5d6691d4d53679212
tjsk002 <tjsk002@naver.com>
2025-05-08 15:42:06 +0900

    feat: coin, market DTO

---
+++ src/main/java/com/coin_notify/coin_notify/data/dto/LikeMarketDto.java
@@ -0,0 +1,12 @@
+package com.coin_notify.coin_notify.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LikeMarketDto {
+    private Boolean isActive;
+}
